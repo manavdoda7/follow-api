12bf37135595175ca57b7ed44fb6ad25156d1f0a
Manav <manavdoda7@gmail.com>
2021-12-22 03:24:43 +0530

    Bug fixes registration route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const db = require('../middlewares/dbconnection')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
 router.get('/', async(req, res)=>{
     console.log('GET /api/users request');
@@ -15,10 +16,52 @@ router.get('/', async(req, res)=>{
         users = users[0]
     } catch(err) {
         console.log('Error in fetching users', err);
-        res.status(408).json({error: 'Couldn\'t fetch users. Please try again after sometime'})
+        res.status(408).json({success:false, error: 'Couldn\'t fetch users. Please try again after sometime'})
         return
     }
     res.status(200).json({success:true, users:users})
+})
+
+router.post('/register', async(req, res)=>{
+    console.log('POST /api/users/request');
+    const {username, email, fName, lName, password} = req.body
+    if( username!=undefined && email!=undefined && fName!=undefined && lName!=undefined && password!=undefined) {
+        let check
+        try {
+            check = await db.promise().query(`select username from user where email="${email}";`)
+            check = check[0]
+            if(check.length) return res.status(403).json({success:true, error:'Email already exists.'})
+        } catch(err) {
+            console.log('Error in checking email validity', err);
+            res.status(408).json({success:false, error:'Couldn\'t register user. Please try again after sometime'})
+            return
+        }
+        try {
+            check = await db.promise().query(`select username from user where username="${username}";`)
+            check = check[0]
+            if(check.length) return res.status(403).json({success:true, error:'Username already exists. Please try again with a diffrent username'})
+        } catch(err) {
+            console.log('Error in checking username validity');
+            res.status(408).json({success:false, error:'Couldn\'t register user. Please try again after sometime'})
+            return
+        }
+        let hashedPassword
+        try {
+            hashedPassword = await bcrypt.hash(password, 11)
+        } catch(err) {
+            console.log('bcrypt error', err);
+            res.status(408).json({success:false, error:'Couldn\'t register user. Please try again after sometime'})
+            return
+        }
+        try {
+            await db.promise().query(`insert into user values("${username}", "${email}", "${fName}", "${lName}", "${password}")`)
+        } catch(err) {
+            console.log('Error in saving to DB.', err);
+            res.status(408).json({success:false, error:'Couldn\'t register user. Please try again after sometime'})
+            return
+        }
+        return res.status(201).json({success:true, message:'User created'})
+    } else return res.status(406).json({success:'false', error:'One or more of the required items missing.'})
 })
 
 module.exports = router
